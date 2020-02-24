cd40b9bf1e93ebbdb19987d1711a399a148bed32
Douglas Abrams <abramsd@mskcc.org>
2020-02-24 16:13:00 -0500

    test

--- tests/jenkins/align/align.sh
+++ tests/jenkins/align/align.sh
@@ -11,15 +11,15 @@ docker run -v $PWD:$PWD -w $PWD $3/azurecli:v0.0.1 \
 echo "\n\n\n\npeofnpowefnjp'ownewfp'on"
 echo $3
 
-#docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
-#  -v /usr/bin/docker:/usr/bin/docker --rm \
-#  $3/single_cell_pipeline:$TAG \
-#  single_cell alignment --input_yaml tests/jenkins/align/inputs.yaml \
-#  --library_id A97318A --maxjobs 4 --nocleanup --sentinel_only  \
-#  --context_config tests/jenkins/align/context_config.yaml \
-#  --submit local --loglevel DEBUG \
-#  --config_override '{"pypeliner_storage_account": "scdnadev"}' --tmpdir ALIGN/temp \
-#  --pipelinedir ALIGN/pipeline --submit local --out_dir ALIGN/output --bams_dir ALIGN/bams
+docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /usr/bin/docker:/usr/bin/docker --rm \
+  AA:$TAG \
+  single_cell alignment --input_yaml tests/jenkins/align/inputs.yaml \
+  --library_id A97318A --maxjobs 4 --nocleanup --sentinel_only  \
+  --context_config tests/jenkins/align/context_config.yaml \
+  --submit local --loglevel DEBUG \
+  --config_override '{"pypeliner_storage_account": "scdnadev"}' --tmpdir ALIGN/temp \
+  --pipelinedir ALIGN/pipeline --submit local --out_dir ALIGN/output --bams_dir ALIGN/bams
 
 
 docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
