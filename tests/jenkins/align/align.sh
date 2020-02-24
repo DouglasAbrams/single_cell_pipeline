6b47abfd8d2b57406b4348c15571cdecdd3d0f98
Douglas Abrams <abramsd@mskcc.org>
2020-02-24 18:15:05 -0500

    test

--- tests/jenkins/align/align.sh
+++ tests/jenkins/align/align.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
-A="singlecellpipelinetest/single_cell_pipeline:v0.5.16"
+
 
 mkdir -p ALIGN/ref_test_data
 
@@ -13,7 +13,7 @@ echo $3
 
 docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
   -v /usr/bin/docker:/usr/bin/docker --rm \
-  $A \
+  $3/single_cell_pipeline:$TAG \
   single_cell alignment --input_yaml tests/jenkins/align/inputs.yaml \
   --library_id A97318A --maxjobs 4 --nocleanup --sentinel_only  \
   --context_config tests/jenkins/align/context_config.yaml \
