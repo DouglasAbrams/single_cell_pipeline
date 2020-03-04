ae468c7637e057ebb0dcb9f7a81af103d37c123b
Douglas Abrams <abramsd@mskcc.org>
2020-03-04 15:19:53 -0500

    G

--- tests/jenkins/variant_calling/variant_calling.sh
+++ tests/jenkins/variant_calling/variant_calling.sh
@@ -10,7 +10,7 @@ sudo mkdir -p VARIANT_CALLING/ref_test_data
 sudo docker run -v $PWD:$PWD -w $PWD $3/azurecli:v0.0.1 \
   az storage blob download-batch -s variant-calling  -d VARIANT_CALLING/ref_test_data/ --account-name $1 --account-key $2
 
-sudo docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
+sudo docker run -w $PWD -v $PWD:$PWD -v $WKDIR:$WKDIR -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
   -v /usr/bin/docker:/usr/bin/docker --rm \
   $3/single_cell_pipeline:$TAG \
   single_cell variant_calling --input_yaml $WKDIR/tests/jenkins/variant_calling/inputs.yaml \
@@ -21,7 +21,7 @@ sudo docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.soc
   --pipelinedir $WKDIR/VARIANT_CALLING/pipeline --submit local --out_dir $WKDIR/VARIANT_CALLING/output \
   --config_override '{"variant_calling": {"chromosomes": ["6", "8", "17"]}}'
 
-docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
+docker run -w $PWD -v $PWD:$PWD -v $WKDIR:$WKDIR -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
   -v /usr/bin/docker:/usr/bin/docker --rm \
   $3/single_cell_pipeline:$TAG \
-  python $WKDIR/tests/jenkins/variant_calling/test_variant_calling.py $WKDIR/VARIANT_CALLING/output $WKDIR/VARIANT_CALLING/ref_test_data/refdata
+  python $WKDIR/tests/jenkins/variant_calling/test_variant_calling.py $WKDIR/VARIANT_CALLING/output $PWD/VARIANT_CALLING/ref_test_data/refdata
