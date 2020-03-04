3fca29ac6ed8a97520771d718ff6329a553c7ef5
Douglas Abrams <abramsd@mskcc.org>
2020-03-04 13:55:07 -0500

    VV

--- tests/jenkins/variant_calling/variant_calling.sh
+++ tests/jenkins/variant_calling/variant_calling.sh
@@ -5,10 +5,10 @@ TAG=`git describe --tags $(git rev-list --tags --max-count=1)`
 cd /mnt
 
 sudo mkdir -p VARIANT_CALLING/ref_test_data
-#
-#docker run -v $PWD:$PWD -w $PWD $3/azurecli:v0.0.1 \
-#  az storage blob download-batch -s variant-calling  -d VARIANT_CALLING/ref_test_data/ --account-name $1 --account-key $2
-#
+
+sudo docker run -v $PWD:$PWD -w $PWD $3/azurecli:v0.0.1 \
+  az storage blob download-batch -s variant-calling  -d VARIANT_CALLING/ref_test_data/ --account-name $1 --account-key $2
+
 #docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
 #  -v /usr/bin/docker:/usr/bin/docker --rm \
 #  $3/single_cell_pipeline:$TAG \
