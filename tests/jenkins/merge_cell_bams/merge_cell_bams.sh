1aa9a1c040ecbadd95af77b7db89b3a40ec5d3fa
Douglas Abrams <abramsd@mskcc.org>
2020-03-06 14:44:47 -0500

    P

--- tests/jenkins/merge_cell_bams/merge_cell_bams.sh
+++ tests/jenkins/merge_cell_bams/merge_cell_bams.sh
@@ -7,17 +7,26 @@ mkdir -p MERGE_CELL_BAMS/ref_test_data
 docker run -v $PWD:$PWD -w $PWD singlecellpipeline/azurecli:v0.0.1 \
   az storage blob download-batch -s merge-bams -d MERGE_CELL_BAMS/ref_test_data --account-name $1 --account-key $2
 
-docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /usr/bin/docker:/usr/bin/docker --rm \
-  $3/single_cell_pipeline:$TAG \
-  single_cell merge_cell_bams --input_yaml tests/jenkins/merge_cell_bams/inputs.yaml \
-  --maxjobs 4 --nocleanup --sentinel_only  \
-  --context_config tests/jenkins/align/context_config.yaml \
-  --submit local --loglevel DEBUG \
-  --tmpdir MERGE_CELL_BAMS/temp \
-  --pipelinedir MERGE_CELL_BAMS/pipeline --submit local --out_dir MERGE_CELL_BAMS/output
+#docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
+#  -v /usr/bin/docker:/usr/bin/docker --rm \
+#  $3/single_cell_pipeline:$TAG \
+#  single_cell merge_cell_bams --input_yaml tests/jenkins/merge_cell_bams/inputs.yaml \
+#  --maxjobs 4 --nocleanup --sentinel_only  \
+#  --context_config tests/jenkins/align/context_config.yaml \
+#  --submit local --loglevel DEBUG \
+#  --tmpdir MERGE_CELL_BAMS/temp \
+#  --pipelinedir MERGE_CELL_BAMS/pipeline --submit local --out_dir MERGE_CELL_BAMS/output
+
+LATEST=v0.0.3
 
 docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
   -v /usr/bin/docker:/usr/bin/docker --rm \
-  $3/single_cell_pipeline:$TAG \
-  python tests/jenkins/merge_cell_bams/test_merge_cell_bams.py MERGE_CELL_BAMS/output MERGE_CELL_BAMS/ref_test_data/refdata
+  $3/samtools:LATEST bash; \
+  for f in MERGE_CELL_BAMS/ref_test_data/testdata/*.bam; do samtools view -f 4 -c $f; done > MERGE_CELL_BAMS/output/unmapped;
+  for f in MERGE_CELL_BAMS/ref_test_data/testdata/*.bam; do samtools view -c $f; done > MERGE_CELL_BAMS/output/mapped;
+| lsMERGE_CELL_BAMS/ref_test_data/testdata/*.bam | awk '{split($0, r, "."); print r[1] }' > MERGE_CELL_BAMS/output/regions
+#
+#docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
+#  -v /usr/bin/docker:/usr/bin/docker --rm \
+#  $3/single_cell_pipeline:$TAG \
+#  python tests/jenkins/merge_cell_bams/test_merge_cell_bams.py MERGE_CELL_BAMS/output MERGE_CELL_BAMS/ref_test_data/refdata
