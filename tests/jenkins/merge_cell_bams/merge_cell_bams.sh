2a93f5dfc879b119d3a667c41f3d33644d3c3096
Douglas Abrams <abramsd@mskcc.org>
2020-03-06 13:21:44 -0500

    P

--- tests/jenkins/merge_cell_bams/merge_cell_bams.sh
+++ tests/jenkins/merge_cell_bams/merge_cell_bams.sh
@@ -6,17 +6,17 @@ mkdir -p MERGE_WGS_BAMS/ref_test_data
 
 docker run -v $PWD:$PWD -w $PWD singlecellpipeline/azurecli:v0.0.1 \
   az storage blob download-batch -s merge-bams -d MERGE_WGS_BAMS/ref_test_data --account-name $1 --account-key $2
-#
-#docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
-#  -v /usr/bin/docker:/usr/bin/docker --rm \
-#  $3/single_cell_pipeline:$TAG \
-#  single_cell merge_cell_bams --input_yaml tests/jenkins/merge_cell_bams/inputs.yaml \
-#  --maxjobs 4 --nocleanup --sentinel_only  \
-#  --context_config tests/jenkins/align/context_config.yaml \
-#  --submit local --loglevel DEBUG \
-#  --tmpdir MERGE_CELL_BAMS/temp \
-#  --pipelinedir MERGE_CELL_BAMS/pipeline --submit local --out_dir MERGE_CELL_BAMS/output
-#
+
+docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /usr/bin/docker:/usr/bin/docker --rm \
+  $3/single_cell_pipeline:$TAG \
+  single_cell merge_cell_bams --input_yaml tests/jenkins/merge_cell_bams/inputs.yaml \
+  --maxjobs 4 --nocleanup --sentinel_only  \
+  --context_config tests/jenkins/align/context_config.yaml \
+  --submit local --loglevel DEBUG \
+  --tmpdir MERGE_CELL_BAMS/temp \
+  --pipelinedir MERGE_CELL_BAMS/pipeline --submit local --out_dir MERGE_CELL_BAMS/output
+
 #docker run -w $PWD -v $PWD:$PWD -v /refdata:/refdata -v /var/run/docker.sock:/var/run/docker.sock \
 #  -v /usr/bin/docker:/usr/bin/docker --rm \
 #  $3/single_cell_pipeline:$TAG \
